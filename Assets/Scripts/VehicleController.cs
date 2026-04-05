8221fcc945e39d4b552440f9fe976d3a1c97abe5
tatemaguire <tatemaguir@gmail.com>
2026-04-05 15:07:41 -0700

    implemented variable acceleration based on direction of velocity

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -4,7 +4,8 @@ using UnityEngine.InputSystem;
 public class VehicleController : MonoBehaviour
 {
     // Vehicle Properties
-    [SerializeField] private float impulse = 4f;
+    [SerializeField] private float minAcceleration = 4f;
+    [SerializeField] private float maxAcceleration = 8f;
     [SerializeField] private float turnrate = 200f;
     
     // References
@@ -26,13 +27,16 @@ public class VehicleController : MonoBehaviour
     
     private void Update()
     {
-        // Apply acceleration and strafing forces
-        Vector3 force = new(
-            _controlVector.y * impulse,
-            0,
-            -_controlVector.x * impulse
-        );
+        // Determine the component of the control vector pointing against velocity
+        float dot = Vector3.Dot(_rigidbody.linearVelocity.normalized, GetSpatialControlVector().normalized);
+        // Use it to interpolate acceleration impulse
+        float impulse = Mathf.Lerp(maxAcceleration, minAcceleration, (dot + 1) / 2f);
+
+        Vector3 force = new(_controlVector.y * impulse, 0, _controlVector.x * -impulse);
         _rigidbody.AddRelativeForce(force);
+        
+        // Debug.DrawRay(transform.position, transform.rotation * force, Color.yellowGreen, 0.5f, false);
+        // Debug.Log(impulse);
         
         // Apply rotation from mouse position
         float dx = (Mouse.current.position.x.value - Screen.width / 2) / turnrate;
@@ -47,5 +51,12 @@ public class VehicleController : MonoBehaviour
     private void OnMove(InputValue action)
     {
         _controlVector = action.Get<Vector2>();
+    }
+
+    private Vector3 GetSpatialControlVector()
+    {
+        Vector3 result = new(_controlVector.y, 0, -_controlVector.x);
+        result = transform.rotation * result;
+        return result;
     }
 }
