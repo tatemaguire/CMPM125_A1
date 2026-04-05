fb84d8654d8fef608391edecb2e0c17775d6b84d
tatemaguire <tatemaguir@gmail.com>
2026-04-05 07:24:14 -0700

    vehicle controller implemented, camera locked on

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class VehicleController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float accelerationForce = 10f;
+    
+    private float _desiredAcceleration;
+    private Rigidbody _rigidbody;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    private void Update()
+    {
+        Vector3 force = new(_desiredAcceleration * accelerationForce, 0, 0);
+        _rigidbody.AddRelativeForce(force);
+    }
+    
+    // Event callback for player input
+    private void OnMove(InputValue action)
+    {
+        var movement = action.Get<Vector2>();
+        _desiredAcceleration = movement.y;
     }
 }
